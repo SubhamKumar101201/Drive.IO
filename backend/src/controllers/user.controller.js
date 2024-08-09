6f9d2eae1e27036b5c5e547317ebdaf24cebd371
SubhamKumar101201 <das.subham101201@gmail.com>
2024-08-09 23:51:46 +0530

    add login controller initiate

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -3,28 +3,30 @@ import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import fs from "fs"
 
 
 
-
-
-const registerUser = asyncHandler( async (req,res) => {
+export const registerUser = asyncHandler( async (req,res) => {
 
     const { name, email, password } = req.body
 
+    const profileImageLocalPath = req.file?.path
+
     if(!name?.trim() || !email?.trim() || !password?.trim()) {
+        fs.unlinkSync(profileImageLocalPath)
         throw new ApiError(400, "Please provide all fields")
     }
 
     const existingUser = await User.findOne({ email })
 
     if (!existingUser) {
+        fs.unlinkSync(profileImageLocalPath)
         throw new ApiError(409, "User with email already exists")
     }
 
-    const profileImageLocalPath = req.file?.path
-
     if(!profileImageLocalPath) {
+        fs.unlinkSync(profileImageLocalPath)
         throw new ApiError(400, "Profile image is required")
     }
    
@@ -55,6 +57,27 @@ const registerUser = asyncHandler( async (req,res) => {
     return res.status(201).json(
         new ApiResponse(200, createdUser, "User registered successfully")
     )
+
+})
+
+export const loginUser = asyncHandler(async(req,res) => {
+    const { email, password } = req.body
+
+    if(!(email || password)) {
+        throw new ApiError(400, "Please provide all fields")
+    }
+
+    const user = await User.findOne({email})
+
+    if(!user) {
+        throw new ApiError(404, "User doesn't exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid password")
+    }    
 
 })
 
--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
 
 cloudinary.config({
 
